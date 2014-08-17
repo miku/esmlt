4938c0849c59c298e14eeb1caf71566499c4b26a
Martin Czygan <martin.czygan@gmail.com>
2014-08-17 16:14:55 +0200

    basic --file and --column implementation

--- cmd/dsqpool/dsqpool.go
+++ cmd/dsqpool/dsqpool.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
 	"log"
 	"net/url"
@@ -13,6 +15,7 @@ import (
 	"github.com/miku/dupsquash"
 )
 
+// Query runs `query` over connection `conn` on `indices` and returns a slice of string slices
 func Query(conn *goes.Connection, indices *[]string, query *map[string]interface{}) [][]string {
 	extraArgs := make(url.Values, 1)
 	searchResults, err := conn.Search(*query, *indices, []string{""}, extraArgs)
@@ -26,6 +29,45 @@ func Query(conn *goes.Connection, indices *[]string, query *map[string]interface
 	return results
 }
 
+// ParseIndices parses strings like `2,4,5` into an int slice
+func ParseIndicesShift(s string, shift int) ([]int, error) {
+	parts := strings.Split(s, ",")
+	var indices []int
+	for _, p := range parts {
+		i, err := strconv.ParseInt(p, 10, 0)
+		if err != nil {
+			return nil, err
+		}
+		indices = append(indices, int(i)+shift)
+	}
+	return indices, nil
+}
+
+// ParseIndices parses strings like `2,4,5` into an int slice
+func ParseIndices(s string) ([]int, error) {
+	return ParseIndicesShift(s, -1)
+}
+
+func ConcatenateValuesNull(values []string, indices []int, nullValue string) (string, error) {
+	var buffer bytes.Buffer
+	for _, i := range indices {
+		if i > len(values)-1 {
+			return "", fmt.Errorf("index %d exceeds array", i)
+		}
+		if values[i] == nullValue {
+			buffer.WriteString("")
+			continue
+		}
+		buffer.WriteString(values[i])
+		buffer.WriteString(" ")
+	}
+	return buffer.String(), nil
+}
+
+func ConcatenateValues(values []string, indices []int) (string, error) {
+	return ConcatenateValuesNull(values, indices, "<NULL>")
+}
+
 func main() {
 	var opts struct {
 		ElasticSearchHost string `long:"host" default:"localhost" description:"elasticsearch host" value-name:"HOST"`
@@ -33,8 +75,10 @@ func main() {
 
 		Like string `short:"l" long:"like" description:"string to compare" value-name:"STRING"`
 
-		LikeFile   string `long:"file" optional:"true" description:"onput file (TSV) with strings to compare" value-name:"FILENAME"`
-		FileColumn string `short:"f" long:"column" default:"1" description:"which column(s) to pick for the comparison" value-name:"COLUMN[S]"`
+		LikeFile      string `short:"i" long:"file" description:"input file (TSV) with strings to compare" value-name:"FILENAME"`
+		FileColumn    string `short:"f" long:"column" default:"1" description:"which column(s) to pick for the comparison" value-name:"COLUMN[S]"`
+		FileDelimiter string `long:"delimiter" default:"\t" description:"column delimiter of the file" value-name:"DELIM"`
+		FileNullValue string `long:"null-value" default:"<NULL>" description:"value that indicates empty value in input file" value-name:"STRING"`
 
 		Index         string `long:"index" description:"index or indices (space separated)" value-name:"NAME[S]"`
 		IndexFields   string `short:"x" default:"content.245.a content.245.b" long:"index-fields"description:"which index fields to use for comparison" value-name:"NAME[S]"`
@@ -66,6 +110,55 @@ func main() {
 	fields := strings.Fields(opts.IndexFields)
 	indices := strings.Fields(opts.Index)
 
+	if opts.LikeFile != "" {
+		if _, err := os.Stat(opts.LikeFile); os.IsNotExist(err) {
+			log.Fatalf("no such file or directory: %s\n", opts.LikeFile)
+		}
+
+		file, err := os.Open(opts.LikeFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+		projector, err := ParseIndices(opts.FileColumn)
+		if err != nil {
+			log.Fatalf("could not parse column indices: %s\n", opts.FileColumn)
+		}
+
+		for scanner.Scan() {
+			values := strings.Split(scanner.Text(), opts.FileDelimiter)
+			likeText, err := ConcatenateValues(values, projector)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			var query = map[string]interface{}{
+				"query": map[string]interface{}{
+					"more_like_this": map[string]interface{}{
+						"fields":          fields,
+						"like_text":       likeText,
+						"min_term_freq":   opts.MinTermFreq,
+						"max_query_terms": opts.MaxQueryTerms,
+					},
+				},
+				"size": opts.Size,
+			}
+
+			results := Query(conn, &indices, &query)
+			for _, result := range results {
+				parts := append(values, result...)
+				fmt.Println(strings.Join(parts, "\t"))
+			}
+		}
+
+		if err := scanner.Err(); err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
+
 	if opts.Like != "" {
 		var query = map[string]interface{}{
 			"query": map[string]interface{}{
@@ -83,5 +176,6 @@ func main() {
 		for _, result := range results {
 			fmt.Println(strings.Join(result, "\t"))
 		}
+		return
 	}
 }
