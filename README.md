02509bd0f6fd5bc79f442c4ac7269ca48e3c8f81
Martin Czygan <martin.czygan@gmail.com>
2014-08-17 13:22:13 +0200

    rename --liketext to --like

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ dsqpool
     Application Options:
           --host=HOST               elasticsearch host (localhost)
           --port=PORT               elasticsearch port (9200)
-      -l, --liketext=STRING         string to compare
+      -l, --like=STRING             string to compare
           --file=FILENAME           onput file (TSV) with strings to compare
       -f, --column=COLUMN[S]        which column(s) to pick for the comparison (1)
           --index=NAME[S]           index or indices (space separated)
@@ -25,12 +25,12 @@ dsqpool
       -V, --version                 show version and exit (false)
       -h, --help                    show this help message (false)
 
-Simplest usage with `--liketext` - run a more like this query, which will
+Start simple with the `--like` option. This will run a *more like this* query, which will
 return document ids, that are similar (in title and subtitle) to the word "Language":
 
-    $ go run cmd/dsqpool/dsqpool.go -l Language
-    ebl EBL1599295  6.759
-    ebl EBL1715045  6.382
+    $ go run cmd/dsqpool/dsqpool.go --like Language
+    ebl EBL1599295      6.759
+    ebl EBL1715045      6.382
     nep 9781108063784   5.320
     nep 9780415576826   5.270
     nep 9780415487528   5.270
--- cmd/dsqpool/dsqpool.go
+++ cmd/dsqpool/dsqpool.go
@@ -1,87 +1,87 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/url"
-    "os"
-    "strconv"
-    "strings"
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+	"strconv"
+	"strings"
 
-    "github.com/belogik/goes"
-    flags "github.com/jessevdk/go-flags"
-    "github.com/miku/dupsquash"
+	"github.com/belogik/goes"
+	flags "github.com/jessevdk/go-flags"
+	"github.com/miku/dupsquash"
 )
 
 func Query(conn *goes.Connection, indices *[]string, query *map[string]interface{}) [][]string {
-    extraArgs := make(url.Values, 1)
-    searchResults, err := conn.Search(*query, *indices, []string{""}, extraArgs)
-    if err != nil {
-        log.Fatalln(err)
-    }
-    results := make([][]string, len(searchResults.Hits.Hits))
-    for i, hit := range searchResults.Hits.Hits {
-        results[i] = []string{hit.Index, hit.Id, strconv.FormatFloat(hit.Score, 'f', 3, 64)}
-    }
-    return results
+	extraArgs := make(url.Values, 1)
+	searchResults, err := conn.Search(*query, *indices, []string{""}, extraArgs)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	results := make([][]string, len(searchResults.Hits.Hits))
+	for i, hit := range searchResults.Hits.Hits {
+		results[i] = []string{hit.Index, hit.Id, strconv.FormatFloat(hit.Score, 'f', 3, 64)}
+	}
+	return results
 }
 
 func main() {
-    var opts struct {
-        ElasticSearchHost string `long:"host" default:"localhost" description:"elasticsearch host" value-name:"HOST"`
-        ElasticSearchPort string `long:"port" default:"9200" description:"elasticsearch port" value-name:"PORT"`
+	var opts struct {
+		ElasticSearchHost string `long:"host" default:"localhost" description:"elasticsearch host" value-name:"HOST"`
+		ElasticSearchPort string `long:"port" default:"9200" description:"elasticsearch port" value-name:"PORT"`
 
-        LikeText string `short:"l" long:"liketext" description:"string to compare" value-name:"STRING"`
+		Like string `short:"l" long:"like" description:"string to compare" value-name:"STRING"`
 
-        LikeFile   string `long:"file" optional:"true" description:"onput file (TSV) with strings to compare" value-name:"FILENAME"`
-        FileColumn string `short:"f" long:"column" default:"1" description:"which column(s) to pick for the comparison" value-name:"COLUMN[S]"`
+		LikeFile   string `long:"file" optional:"true" description:"onput file (TSV) with strings to compare" value-name:"FILENAME"`
+		FileColumn string `short:"f" long:"column" default:"1" description:"which column(s) to pick for the comparison" value-name:"COLUMN[S]"`
 
-        Index         string `long:"index" description:"index or indices (space separated)" value-name:"NAME[S]"`
-        IndexFields   string `short:"x" default:"content.245.a content.245.b" long:"index-fields"description:"which index fields to use for comparison" value-name:"NAME[S]"`
-        MinTermFreq   int    `long:"min-term-freq" description:"passed on lucene option" default:"1" value-name:"N"`
-        MaxQueryTerms int    `long:"max-query-terms" description:"passed on lucene option" default:"25" value-name:"N"`
-        Size          int    `short:"s" long:"size" description:"number of results per query" default:"5" value-name:"N"`
+		Index         string `long:"index" description:"index or indices (space separated)" value-name:"NAME[S]"`
+		IndexFields   string `short:"x" default:"content.245.a content.245.b" long:"index-fields"description:"which index fields to use for comparison" value-name:"NAME[S]"`
+		MinTermFreq   int    `long:"min-term-freq" description:"passed on lucene option" default:"1" value-name:"N"`
+		MaxQueryTerms int    `long:"max-query-terms" description:"passed on lucene option" default:"25" value-name:"N"`
+		Size          int    `short:"s" long:"size" description:"number of results per query" default:"5" value-name:"N"`
 
-        ShowVersion bool `short:"V" default:"false" long:"version" description:"show version and exit"`
-        ShowHelp    bool `short:"h" default:"false" long:"help" description:"show this help message"`
-    }
-    argparser := flags.NewParser(&opts, flags.PrintErrors|flags.PassDoubleDash)
-    _, err := argparser.Parse()
-    if err != nil {
-        log.Fatal(err)
-    }
+		ShowVersion bool `short:"V" default:"false" long:"version" description:"show version and exit"`
+		ShowHelp    bool `short:"h" default:"false" long:"help" description:"show this help message"`
+	}
+	argparser := flags.NewParser(&opts, flags.PrintErrors|flags.PassDoubleDash)
+	_, err := argparser.Parse()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    if opts.ShowVersion {
-        fmt.Printf("%s\n", dupsquash.AppVersion)
-        return
-    }
+	if opts.ShowVersion {
+		fmt.Printf("%s\n", dupsquash.AppVersion)
+		return
+	}
 
-    argparser.Usage = fmt.Sprintf("[OPTIONS]")
-    if opts.ShowHelp {
-        argparser.WriteHelp(os.Stdout)
-        return
-    }
+	argparser.Usage = fmt.Sprintf("[OPTIONS]")
+	if opts.ShowHelp {
+		argparser.WriteHelp(os.Stdout)
+		return
+	}
 
-    conn := goes.NewConnection(opts.ElasticSearchHost, opts.ElasticSearchPort)
-    fields := strings.Fields(opts.IndexFields)
-    indices := strings.Fields(opts.Index)
+	conn := goes.NewConnection(opts.ElasticSearchHost, opts.ElasticSearchPort)
+	fields := strings.Fields(opts.IndexFields)
+	indices := strings.Fields(opts.Index)
 
-    if opts.LikeText != "" {
-        var query = map[string]interface{}{
-            "query": map[string]interface{}{
-                "more_like_this": map[string]interface{}{
-                    "fields":          fields,
-                    "like_text":       opts.LikeText,
-                    "min_term_freq":   opts.MinTermFreq,
-                    "max_query_terms": opts.MaxQueryTerms,
-                },
-            },
-            "size": opts.Size,
-        }
+	if opts.Like != "" {
+		var query = map[string]interface{}{
+			"query": map[string]interface{}{
+				"more_like_this": map[string]interface{}{
+					"fields":          fields,
+					"like_text":       opts.Like,
+					"min_term_freq":   opts.MinTermFreq,
+					"max_query_terms": opts.MaxQueryTerms,
+				},
+			},
+			"size": opts.Size,
+		}
 
-        results := Query(conn, &indices, &query)
-        for _, result := range results {
-            fmt.Println(strings.Join(result, "\t"))
-        }
-    }
+		results := Query(conn, &indices, &query)
+		for _, result := range results {
+			fmt.Println(strings.Join(result, "\t"))
+		}
+	}
 }
